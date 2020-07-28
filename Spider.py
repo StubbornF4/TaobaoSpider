b0d2f21f293d12ef145a7f6183b64c8a8373f0df
John <lichunxiao0404>
2020-07-28 16:21:14 +0800

    增加转页

--- Spider.py
+++ Spider.py
@@ -44,9 +44,7 @@ def search(url, keyword):
         submit.click()
 
 def parse_page():
-    wait.until = (
-        EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-itemlist'))
-    )
+    #利用Pyquery 查找找到的页面元素
     html = browser.page_source
     doc = pq(html)
     items = doc('#mainsrp-itemlist .items .item').items()
@@ -57,6 +55,7 @@ def parse_page():
             'price': item.find('.price').text(),
             'shop': item.find('.shop').text(),
         }
+
         product_value.append(product)
     print(product_value)
     return product_value
@@ -77,18 +76,43 @@ def download_message(message):
 
     wb.save('products.xlsx')
 
-def main():
-    #get_cookie(url)
-    #set_cookie(url)
+def turn_page(page):
+    try:
+        browser.refresh()
+        input = wait.until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > div.form > input'))
+        )
+        submit = wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, '#mainsrp-pager > div > div > div > div.form > span.btn.J_Submit'))
+        )
+        #将页面的专业输入框内容清楚，再输入页码
+        input.clear()
+        input.send_keys(page)
+        submit.click()
+        #等到当前页面数字，正好对应转页的数字，否则引发错误，重新转页
+        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#mainsrp-pager li.item.active> span'), str(page)))
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.m-itemlist .items .item')))
+    except TimeoutException:
+        turn_page(page)
 
+def main():
     browser.get(url)
     print('请手动登陆用户')
     time.sleep(40)
 
     print('开始搜索页面')
     search(url, 'Macbook PRO')
+    #获取产品信息
     product_message = parse_page()
+    #获取2~4页的信息
+    for i in range(2, 5):
+        #转页
+        turn_page(i)
+        product_message += parse_page()
+
+    #将产品信息存储到excel中
     download_message(product_message)
+
 
 if __name__ == '__main__':
     main()
